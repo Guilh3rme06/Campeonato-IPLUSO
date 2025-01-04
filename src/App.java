27aab6750f8844a17b55d2deb53bb45bfb95e6f2
Gustavo Rodrigues <gurodriguesalves12@gmail.com>
2025-01-04 21:15:33 +0000

    Update Files

--- src/App.java
+++ src/App.java
@@ -15,10 +15,11 @@ public class App {
             System.out.println("3 - Gerar/Controlar Torneio");
             System.out.println("4 - Visualizar Campeonato");
             System.out.println("5 - Listar Jogadores e Árbitros");
-            System.out.println("6 - Sair");
+            System.out.println("6 - Criar e Exibir Partida Singular");
+            System.out.println("7 - Sair");
 
             int opcao = scanner.nextInt();
-            scanner.nextLine();
+            scanner.nextLine(); // Consumir a nova linha restante
 
             switch (opcao) {
                 case 1:
@@ -42,6 +43,33 @@ public class App {
                 case 4:
                     break;
                 case 5:
+                    // Mostrar Jogadores e Árbitros
+                    System.out.println("Jogadores Registrados:");
+                    for (Jogador j : jogadores) {
+                        j.exibirJogador();
+                        System.out.println();
+                    }
+                    System.out.println("Árbitros Registrados:");
+                    for (Arbitro a : arbitros) {
+                        a.exibirArbitro();
+                        System.out.println();
+                    }
+                    break;
+                case 6:
+                    // Partida Singular
+                    if (jogadores.size() < 2 || arbitros.isEmpty()) {
+                        System.out.println("Não há jogadores ou árbitros suficientes para criar uma partida.");
+                    } else {
+                        Jogador jogador1 = jogadores.get(0);
+                        Jogador jogador2 = jogadores.get(1);
+                        Arbitro arbitroPartida = arbitros.get(0);
+
+                        PartidaSingulares partida = new PartidaSingulares(jogador1, jogador2, arbitroPartida);
+                        partida.partidaSingular();
+                    }
+                    break;
+                case 7:
+                    System.out.println("Saindo...");
                     scanner.close();
                     return;
                 default:
--- src/PartidaSingulares.java
+++ src/PartidaSingulares.java
@@ -1,3 +1,56 @@
-public class PartidaSingulares {
-    
+import java.util.Random;
+
+public class PartidaSingulares{
+    private Jogador jogador1;
+    private Jogador jogador2;
+    private Arbitro arbitro;
+
+    // Construtor
+    public PartidaSingulares(Jogador jogador1, Jogador jogador2, Arbitro arbitro) {
+        this.jogador1 = jogador1;
+        this.jogador2 = jogador2;
+        this.arbitro = arbitro;
+    }
+
+    // Getters
+    public Jogador getJogador1() {
+        return jogador1;
+    }
+
+    public Jogador getJogador2() {
+        return jogador2;
+    }
+
+    public Arbitro getArbitro() {
+        return arbitro;
+    }
+
+    // Setters
+    public void setJogador1(Jogador jogador1) {
+        this.jogador1 = jogador1;
+    }
+
+    public void setJogador2(Jogador jogador2) {
+        this.jogador2 = jogador2;
+    }
+
+    public void setArbitro(Arbitro arbitro) {
+        this.arbitro = arbitro;
+    }
+
+    public void partidaSingular(){
+        this.jogador1.setPartidasJogadas(jogador1.getPartidasJogadas() + 1);
+        this.jogador2.setPartidasJogadas(jogador2.getPartidasJogadas() + 1);
+
+        Random vencedorPartidaSingular = new Random();
+        int vencedor = vencedorPartidaSingular.nextInt(2) + 1;
+
+        if(vencedor == 1) {
+            System.out.println("O vencedor da Partida Singular é: " + this.jogador1.getNome());
+            this.jogador1.setRankings(jogador1.getRankings() + 3);
+        } else {
+            System.out.println("O vencedor da Partida Singular é: " + this.jogador2.getNome());
+            this.jogador2.setRankings(jogador2.getRankings() + 3);
+        }
+    }
 }
